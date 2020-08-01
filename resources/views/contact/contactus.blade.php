3fe5e1792aa1087366b420161d8b76648da8fada
Derockypeter <amaizupeter@yahoo.com>
2020-08-01 18:18:54 +0100

    fixed contact us

--- resources/views/contact/contactus.blade.php
+++ resources/views/contact/contactus.blade.php
@@ -1,10 +1,9 @@
-<center>
-    <h2>Hello Mankind,</h2>
-    <p class="lead">You received an email from : {{ $name }}</p>
-    <p class="lead">Here are the details:</p>
-    <p>Name: {{ $name }}</p>
-    <p>Email: {{ $email }}</p>
-    <p>Subject: {{ $subject }}</p>
-    <p>Message: {{ $user_message }}</p>
-    <p>Thank You</p>
-</center>
+
+<h2>Hello Mankind,</h2>
+<p class="lead">You received an email from : {{ $name }}</p>
+<p class="lead">Here are the details:</p>
+<p>Name: {{ $name }}</p>
+<p>Email: {{ $email }}</p>
+<p>Subject: {{ $subject }}</p>
+<p>Message: {{ $user_message }}</p>
+<p>Thank You</p>
